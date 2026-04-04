637aceb78ad8de4b544b627211531e5cc310a66f
luthfiupb5 <luthfimonu1@gmail.com>
2026-04-04 14:18:44 +0530

    chore: sync latest changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d47a0fae'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1mnr0fdrm68"
+    "revision": "0.fo85478t1po"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
